0996e41e5ebfc26cc56dfc6a00887454be7ec2de
Anna Karingal <anna.karingal@gmail.com>
2015-04-19 13:59:53 -0400

    DiskDrive now own class with priority queue. Merged CPU & ready queu classes, implemented with priority queue

--- devices.py
+++ devices.py
@@ -4,7 +4,7 @@
 # Author:           Anna Cristina Karingal
 # Name:             devices.py
 # Created:          February 27, 2015
-# Last Updated:     March 5, 2015
+# Last Updated:     April 19, 2015
 # Description:      Classes for different devices on the system. Contains
 #                   methods allowing user to see/change what process(es) a
 #                   device is running or are in the device queue. 
@@ -16,15 +16,15 @@ import msg
 from queues import DeviceQueue
 from pcb import PCB
 
-class Device(DeviceQueue):
+class Device(FIFOQueue):
 
     def __init__(self, dname, dtype): 
         """
         Initializes new device with device name & device type, and new
-        empty queue
+        empty FIFO queue
 
         """ 
-        DeviceQueue.__init__(self)
+        FIFOQueue.__init__(self)
         self._dev_name = dname
         self._dev_type = dtype
 
@@ -33,7 +33,7 @@ class Device(DeviceQueue):
     def enqueue(self, proc):
         """ Add process to end of queue """
         proc.set_proc_loc(self._dev_name)
-        DeviceQueue.enqueue(self,proc)
+        FIFOQueue.enqueue(self,proc)
         print proc.status()
 
     ## Methods to print device in human readable form to console
@@ -49,7 +49,7 @@ class Device(DeviceQueue):
     def snapshot(self):
         """ Prints all processes in queue to console """
         print msg.snapshot_header(self._dev_name)
-        DeviceQueue.snapshot(self)
+        FIFOQueue.snapshot(self)
 
     ## Methods to check/return device name/type
 
@@ -65,18 +65,56 @@ class Device(DeviceQueue):
     def get_dev_name(self):
         return self._dev_name
 
+class DiskDrive(PriorityQueue):
+    """
+    Initializes new disk drive with device name and two empty queues
+    to implement FSCAN scheduling
+    """ 
 
-class CPU(): 
+    def __init__(self, dname, cyl):
+        self._dev_type = "Disk Drive"
+        self._dname = dname
+        self._dname = dname
+        PriorityQueue.__init__(self) # A first priority queue
+        self._q2 = PriorityQueue() # A second priority queue
 
-    def __init__(self):
-        """ Initializes CPU with no active processes """ 
+    def get_num_cylinders(self):
+        return self._cylinders
+
+    def is_device_name(self, query_name):
+        return True if self._dev_name == query_name else False
+
+    def get_dev_name(self):
+        return self._dev_name
+
+class CPU(PriorityQueue): 
+
+    def __init__(self, a):
+        """ Initializes CPU with no active processes and empty Priority Queue""" 
         self.active = None
+        PriorityQueue.__init__(self)
+        self._alpha = a 
 
     def empty(self):
         """ Returns true if no active process in CPU """ 
         return False if self.active else True
 
     ## Methods to modify active process in CPU
+
+    def enqueue(self,proc):
+        """
+        Adds process to back of ready queue and updates PCB 
+        status/location 
+
+        """
+        proc.set_proc_loc("Ready")
+        PriorityQueue.enqueue(self,proc)
+        print proc.status()
+
+    def snapshot(self):
+        """ Prints processes in ready queue with header """
+        print msg.snapshot_header("ready")
+        PriorityQueue.snapshot(self)
 
     def set_process(self, proc):
         """ Sets active process in CPU to given process """
