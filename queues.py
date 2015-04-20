e06591be7f361f6cd5da112b15f174f59d9b46ca
Anna Karingal <anna.karingal@gmail.com>
2015-04-20 12:23:03 -0400

    Added FrozenQueue except

--- queues.py
+++ queues.py
@@ -4,7 +4,7 @@
 # Author:           Anna Cristina Karingal
 # Name:             queues.py
 # Created:          February 27, 2015
-# Last Updated:     April 9, 2015
+# Last Updated:     April 20, 2015
 # Description:      Classes for different types of queues in the system.
 #                   Contains methods allowing user to view what is in the
 #                   queue and enqueue or dequeue a process.
@@ -32,7 +32,6 @@ class Queue:
         """
         Prints a paginated view of processes & process parameters in 
         queue.
-
         """
         if self._q: 
             
@@ -66,9 +65,6 @@ class Queue:
 
                 start += max_height
                 end += max_height
-
-
-
         else:
 
             print '{:^78}'.format("EMPTY: No processes in queue") + "\n"
@@ -97,11 +93,12 @@ class PriorityQueue(Queue):
 
     def __init__(self, f = False):
         """
-        Initialize with empty min heap
+        Initialize with empty min heap, and an unfrozen queue
         """
         self._q = []
         self._frozen = f
 
+    # Methods to freeze/unfreeze queue
     def is_frozen(self):
         return self._frozen
 
@@ -111,17 +108,29 @@ class PriorityQueue(Queue):
     def unfreeze(self):
         self._frozen = False
 
+    # Enqueue/Dequeue methods
     def enqueue(self, proc):
         """
-        Add to heap maintaining heap order property
+        Add to heap maintaining heap order property. Only add to heap if queue
+        is not frozen.
         """
         if not self._frozen:
             heapq.heappush(self._q,proc)
-        else: # TO DO : FIND BETTER ERROR
-            raise IndexError
+        else: 
+            raise FrozenQueue("Cannot enqueue to frozen queue")
 
     def dequeue(self):
         """
         Remove and return task with lowest priority
         """
         return heapq.heappop(self._q)
+
+
+class FrozenQueue(Exception):
+    """
+    Exception raised for trying to enqueue to frozen queue 
+    """
+    def __init__(self,value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
