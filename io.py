19ab86ce05d4a1c4c22b238cba2299d9133a7b2b
Anna Karingal <anna.karingal@gmail.com>
2015-05-10 12:11:58 -0400

    Added get valid hex method

--- io.py
+++ io.py
@@ -4,11 +4,12 @@
 # Author:           Anna Cristina Karingal
 # Name:             msg.py
 # Created:          March 1, 2015
-# Last Updated:     May 4, 2015
+# Last Updated:     May 10, 2015
 # Description:      Contains methods to display formatted output to terminal
 #					and check input for validity
 
 screen_width = 78
+import re
 
 ## Formatting Input
 
@@ -50,8 +51,21 @@ def get_valid_int(prompt, err_msg="Please enter a valid positive integer"):
 				is_int = True
 				return num
 			else: raise ValueError
-		except:
+		except ValueError:
 			print err(err_msg)
+
+def get_valid_hex(prompt, err_msg="Please enter a valid hexadecimal number"):
+    is_hex = False
+    while not is_hex:
+        try:
+            num = raw_input(prompt + " >>> ")
+            if re.match("[0-9a-fA-F]", str(num)):
+                is_hex = True
+                return num
+            else:
+                raise ValueError
+        except Exception as e:
+            print err(err_msg)
 
 def get_pow_two(prompt, err_msg="Please enter a valid power of two"):
 	is_pow = False
